15734d9b527cadeb0f52aecad4183cb8dd7ff5b5
SergeyEmelyanov <sergej-rabota@yandex.ru>
2023-05-08 05:32:39 +0300

    Вариант 1

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,30 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        UserService userService = new UserServiceImpl();
+        userService.createUsersTable();//1. Создание таблицы User(ов)
+        String name;
+        String lastName;
+        for (byte i = 1; i <= 4; i++) {
+            name = "Name" + i;
+            lastName = "Lastname" + i;
+            userService.saveUser(name, lastName, i);//2.Добавление 4 User(ов) в таблицу с данными на свой выбор.
+            // После каждого добавления должен быть вывод в консоль ( User с именем – name добавлен в базу данных )
+            System.out.println("User с именем – " + name + " добавлен в базу данных");
+        }
+        List<User> list = userService.getAllUsers(); //3.Получение всех User из базы и вывод в консоль
+        // ( должен быть переопределен toString в классе User)
+        for (User user : list) {
+            System.out.println(user);
+        }
+        userService.cleanUsersTable();//4. Очистка таблицы User(ов)
+        userService.dropUsersTable();//5. Удаление таблицы
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoJDBCImpl.java
@@ -1,35 +1,120 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoJDBCImpl implements UserDao {
-    public UserDaoJDBCImpl() {
+    private final String CREATE_TABLE = """
+            CREATE TABLE IF NOT EXISTS `databasepp`.`users` (
+            `id` BIGINT NOT NULL AUTO_INCREMENT,
+            `name` VARCHAR(45) NULL,
+            `lastname` VARCHAR(45) NULL,
+            `age` TINYINT(3) NULL,
+            PRIMARY KEY (`id`),
+            UNIQUE INDEX `idusers_UNIQUE` (`id` ASC) VISIBLE)
+            ENGINE = InnoDB
+            DEFAULT CHARACTER SET = utf8;""";
+    private final String DROP_TABLE = "DROP TABLE IF EXISTS users";
+    private final String INSERT_INTO = "INSERT INTO users (name,lastname,age) VALUES (?,?,?)";
+    private final String DELETE_BY_ID = "DELETE FROM users WHERE id=?";
+    private final String SELECT_ALL = "SELECT * FROM users";
+    private final String DELETE_ALL = "DELETE FROM users";
 
+    public UserDaoJDBCImpl() {
     }
 
     public void createUsersTable() {
-
+        try (Connection connection = Util.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(CREATE_TABLE)) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void dropUsersTable() {
-
+        try (Connection connection = Util.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(DROP_TABLE)) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public void saveUser(String name, String lastName, byte age) {
+        createUsersTable();
+        try (Connection connection = Util.getConnection()) {
+            connection.setAutoCommit(false);
+            try (PreparedStatement preparedStatement = connection.prepareStatement(INSERT_INTO)) {
+                preparedStatement.setString(1, name);
+                preparedStatement.setString(2, lastName);
+                preparedStatement.setByte(3, age);
+                preparedStatement.addBatch();
+                preparedStatement.executeBatch();
+                connection.commit();
 
+            } catch (SQLException e) {
+                connection.rollback();
+                throw new RuntimeException(e);
+            }
+        } catch (SQLException e) {
+            System.err.println("Соединение потеряно");
+            e.printStackTrace();
+        }
     }
 
     public void removeUserById(long id) {
-
+        createUsersTable();
+        try (Connection connection = Util.getConnection()) {
+            connection.setAutoCommit(false);
+            try (PreparedStatement preparedStatement = connection.prepareStatement(DELETE_BY_ID)) {
+                preparedStatement.setLong(1, id);
+                preparedStatement.execute();
+                connection.commit();
+            } catch (SQLException e) {
+                connection.rollback();
+                throw new RuntimeException(e);
+            }
+        } catch (SQLException e) {
+            System.err.println("Соединение потеряно");
+            e.printStackTrace();
+        }
     }
 
+
     public List<User> getAllUsers() {
-        return null;
+        createUsersTable();
+        List<User> users = new ArrayList<>();
+        User user;
+        try (Connection connection = Util.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(DELETE_BY_ID);
+             ResultSet resultSet = preparedStatement.executeQuery(SELECT_ALL)) {
+            while (resultSet.next()) {
+                user = new User(resultSet.getString("name"), resultSet.getString("lastname"),
+                        resultSet.getByte("age"));
+                user.setId(resultSet.getLong("id"));
+                users.add(user);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException();
+        }
+        return users;
     }
 
     public void cleanUsersTable() {
-
+        createUsersTable();
+        try (Connection connection = Util.getConnection();
+             PreparedStatement preparedStatement = connection.prepareStatement(DELETE_ALL)) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException();
+        }
     }
 }
